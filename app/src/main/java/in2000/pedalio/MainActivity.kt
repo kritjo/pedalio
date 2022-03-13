bcc06dfa184fb1618d950563c26d2efc0fda4cf0
Kristian Tjelta Johansen <kritjo@kritjo.com>
2022-03-13 17:39:14 +0100

    Repository implementation of Nowcast

--- app/src/main/java/in2000/pedalio/MainActivity.kt
+++ app/src/main/java/in2000/pedalio/MainActivity.kt
@@ -2,23 +2,10 @@ package in2000.pedalio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import in2000.pedalio.data.Endpoints
-import in2000.pedalio.data.weather.source.NowcastSource
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        runBlocking {
-            withContext(Dispatchers.IO) {
-                Log.d("API thing",
-                    NowcastSource.getNowcast(Endpoints.NOWCAST_COMPLETE, 59.923050, 10.731900).toString()
-                )
-            }
-        }
     }
 }
--- app/src/main/java/in2000/pedalio/data/weather/impl/NowcastRepository.kt
+++ app/src/main/java/in2000/pedalio/data/weather/impl/NowcastRepository.kt
@@ -1,38 +1,132 @@
 package in2000.pedalio.data.weather.impl
 
 import in2000.pedalio.data.weather.WeatherRepository
+import in2000.pedalio.data.weather.source.NowcastSource
+import kotlinx.coroutines.runBlocking
+import kotlin.math.floor
 
-class NowcastRepository(lat: Double, lon: Double) : WeatherRepository(lat, lon) {
-    override fun getTemp(timeDelta: Int): Double {
-        TODO("Not yet implemented")
+/**
+ * Implementation of Weather using Nowcast API. Provides no caching.
+ * Source of truth: Nowcast API.
+ */
+class NowcastRepository(
+    private val endpoint: String
+) : WeatherRepository() {
+    override fun getTemp(lat: Double,
+                         lon: Double,
+                         timeDelta: Int): Double? {
+        return runBlocking {
+            NowcastSource
+                .getNowcast(endpoint, lat, lon)
+                .properties
+                .timeseries[floor((timeDelta / 5).toDouble()).toInt()]
+                .data
+                .instant
+                .details
+                .air_temperature
+        }
     }
 
-    override fun getPercipitationRate(timeDelta: Int): Double {
-        TODO("Not yet implemented")
+    override fun getPercipitationRate(lat: Double,
+                                      lon: Double,
+                                      timeDelta: Int): Double? {
+        return runBlocking {
+            NowcastSource
+                .getNowcast(endpoint, lat, lon)
+                .properties
+                .timeseries[floor((timeDelta / 5).toDouble()).toInt()]
+                .data
+                .instant
+                .details
+                .precipitation_rate
+        }
     }
 
-    override fun getPercipitation(timeDelta: Int): Double {
-        TODO("Not yet implemented")
+    override fun getPercipitation(lat: Double,
+                                  lon: Double,
+                                  timeDelta: Int): Double? {
+        return runBlocking {
+            NowcastSource
+                .getNowcast(endpoint, lat, lon)
+                .properties
+                .timeseries[floor((timeDelta / 5).toDouble()).toInt()]
+                .data
+                .instant
+                .details
+                .precipitation_amount
+        }
     }
 
-    override fun getRelativeHumidity(timeDelta: Int): Double {
-        TODO("Not yet implemented")
+    override fun getRelativeHumidity(lat: Double,
+                                     lon: Double,
+                                     timeDelta: Int): Double? {
+        return runBlocking {
+            NowcastSource
+                .getNowcast(endpoint, lat, lon)
+                .properties
+                .timeseries[floor((timeDelta / 5).toDouble()).toInt()]
+                .data
+                .instant
+                .details
+                .relative_humidity
+        }
     }
 
-    override fun getWindDirection(timeDelta: Int): Double {
-        TODO("Not yet implemented")
+    override fun getWindDirection(lat: Double,
+                                  lon: Double,
+                                  timeDelta: Int): Double? {
+        return runBlocking {
+            NowcastSource
+                .getNowcast(endpoint, lat, lon)
+                .properties
+                .timeseries[floor((timeDelta / 5).toDouble()).toInt()]
+                .data
+                .instant
+                .details
+                .wind_from_direction
+        }
     }
 
-    override fun getWindSpeed(timeDelta: Int): Double {
-        TODO("Not yet implemented")
+    override fun getWindSpeed(lat: Double,
+                              lon: Double,
+                              timeDelta: Int): Double? {
+        return runBlocking {
+            NowcastSource
+                .getNowcast(endpoint, lat, lon)
+                .properties
+                .timeseries[floor((timeDelta / 5).toDouble()).toInt()]
+                .data
+                .instant
+                .details
+                .wind_speed
+        }
     }
 
-    override fun getGustSpeed(timeDelta: Int): Double {
-        TODO("Not yet implemented")
+    override fun getGustSpeed(lat: Double,
+                              lon: Double,
+                              timeDelta: Int): Double? {
+        return runBlocking {
+            NowcastSource
+                .getNowcast(endpoint, lat, lon)
+                .properties
+                .timeseries[floor((timeDelta / 5).toDouble()).toInt()]
+                .data
+                .instant
+                .details
+                .wind_speed_of_gust
+        }
     }
 
-    override fun radarCoverage(): Boolean {
-        TODO("Not yet implemented")
+    override fun radarCoverage(lat: Double,
+                               lon: Double): Boolean {
+        val cov = runBlocking {
+            NowcastSource
+                .getNowcast(endpoint, lat, lon)
+                .properties
+                .meta
+                .radar_coverage
+        }
+        if (cov == "ok") return true
+        return false
     }
-
 }
