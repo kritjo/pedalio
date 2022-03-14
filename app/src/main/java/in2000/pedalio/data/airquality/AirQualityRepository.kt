390064cd7c8107a2664d3a91cbafc05be7021aa6
Kristian Tjelta Johansen <kritjo@kritjo.com>
2022-03-14 20:30:23 +0100

    AQF implementation

--- app/src/main/java/in2000/pedalio/data/airquality/AirQualityRepository.kt
+++ app/src/main/java/in2000/pedalio/data/airquality/AirQualityRepository.kt
@@ -6,21 +6,31 @@ abstract class AirQualityRepository {
      * @param lat latitude
      * @param lon longitude
      * @param timeDelta in minutes
-     * @return Gets NOX value for the given location and timeDelta
+     * @return Gets NOX value for the given location and timeDelta in ug/m3
      */
-    abstract fun getNOX(lat: Double, lon: Double, timeDelta: Int = 0): Double
+    abstract suspend fun getNOX(lat: Double, lon: Double, timeDelta: Int = 0): Double
+
     /**
      * @param lat latitude
      * @param lon longitude
      * @param timeDelta in minutes
-     * @return Gets Particulate Matter value for the given location and timeDelta
+     * @return Gets Particulate Matter value for the given location and timeDelta in ug/m3
      */
-    abstract fun getPM(lat: Double, lon: Double, timeDelta: Int = 0): Double
+    abstract suspend fun getPM10(lat: Double, lon: Double, timeDelta: Int = 0): Double
+
     /**
      * @param lat latitude
      * @param lon longitude
      * @param timeDelta in minutes
-     * @return Gets Air Quality Index value for the given location and timeDelta
+     * @return Gets Particulate Matter value for the given location and timeDelta in ug/m3
      */
-    abstract fun getAQI(lat: Double, lon: Double, timeDelta: Int = 0): Double
+    abstract suspend fun getPM25(lat: Double, lon: Double, timeDelta: Int = 0): Double
+
+    /**
+     * @param lat latitude
+     * @param lon longitude
+     * @param timeDelta in minutes
+     * @return Gets Air Quality Index value for the given location and timeDelta from 1 (min) to 5 (max)
+     */
+    abstract suspend fun getAQI(lat: Double, lon: Double, timeDelta: Int = 0): Double
 }
