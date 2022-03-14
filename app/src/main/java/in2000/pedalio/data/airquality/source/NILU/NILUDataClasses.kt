bf62c5b1ac1a0b80852437586138b56e973b8020
Jonas Lien Sampaio Da Silva <jonaslsa@ulrik.uio.no>
2022-03-14 18:07:37 +0100

    Air quality source from NILU

---
+++ app/src/main/java/in2000/pedalio/data/airquality/source/NILU/NILUDataClasses.kt
@@ -0,0 +1,25 @@
+package in2000.pedalio.data.airquality.source.NILU
+
+class NILUDataClass : ArrayList<NILUDataItem>()
+
+data class NILUDataItem(
+    val area: String = "",
+    val color: String = "",
+    val component: String = "",
+    val eoi: String = "",
+    val fromTime: String = "",
+    val id: Int = 0,
+    val index: Int = 0,
+    val isValid: Boolean = false,
+    val isVisible: Boolean = false,
+    val latitude: Double = 0.0,
+    val longitude: Double = 0.0,
+    val municipality: String = "",
+    val station: String = "",
+    val timestep: Int = 0,
+    val toTime: String = "",
+    val type: String = "",
+    val unit: String = "",
+    val value: Double = 0.0,
+    val zone: String = ""
+)
