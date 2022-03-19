2b81b1124cf325d354fc0b831729e7dd5397fa4b
igormomcilovic <igormo@hotmail.com>
2022-03-19 15:05:18 +0100

    Added map Fragment

--- app/src/main/java/in2000/pedalio/MainActivity.kt
+++ app/src/main/java/in2000/pedalio/MainActivity.kt
@@ -13,34 +13,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
-        mapView = findViewById<MapView>(R.id.map_fragment);
-        mapView.addOnMapReadyCallback { tomtomMap ->
-            val osloTopLeft = LatLng(59.92194833346756, 10.718651865762322)
-            val osloBottomRight = LatLng(59.903811901433464, 10.764555190843193)
-            val boundingBox = BoundingBox(osloTopLeft, osloBottomRight)
-            val oslo = LatLng(59.9440703, 10.7189933)
-            val focusArea: CameraFocusArea = CameraFocusArea.Builder(boundingBox)
-                .pitch(5.0)
-                .bearing(MapConstants.ORIENTATION_NORTH.toDouble())
-                .build()
-
-            tomtomMap.centerOn(focusArea)
-            tomtomMap.addMarker(MarkerBuilder(oslo))
-            tomtomMap.isMyLocationEnabled = true
-            val location = tomtomMap.userLocation
-
-
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        mapView.onResume()
-    }
-
-    override fun onPause() {
-        mapView.onPause()
-        super.onPause()
-
     }
 }
