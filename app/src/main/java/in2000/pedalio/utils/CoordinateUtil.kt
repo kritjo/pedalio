bca16728e3bbfb5f7eaf0a557872d18c7bea6f0f
Jonas <jonasvox.2014@gmail.com>
2022-03-21 21:08:08 +0100

    Function to calculate distance between two coordinates

--- app/src/main/java/in2000/pedalio/utils/CoordinateUtil.kt
+++ app/src/main/java/in2000/pedalio/utils/CoordinateUtil.kt
@@ -1,6 +1,10 @@
 package in2000.pedalio.utils
 
 import com.tomtom.online.sdk.common.location.LatLng
+import kotlin.math.asin
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 class CoordinateUtil {
     companion object{
@@ -15,6 +19,19 @@ class CoordinateUtil {
                 result.add(points[i])
             }
             return result
+        }
+
+        // https://www.geeksforgeeks.org/program-distance-two-points-earth/
+        fun calcDistanceBetweenTwoCoordinates(a : LatLng, b : LatLng) : Double {
+            val lon1 = Math.toRadians(a.longitude)
+            val lon2 = Math.toRadians(b.longitude)
+            val lat1 = Math.toRadians(a.latitude)
+            val lat2 = Math.toRadians(b.latitude)
+            val dlon = lon2 - lon1
+            val dlat = lat2 - lat1
+            val aa = Math.pow(sin(dlat / 2), 2.0) + cos(lat1) * cos(lat2) * Math.pow(sin(dlon / 2), 2.0)
+            val c = 2 * asin(sqrt(aa))
+            return 6371 * c
         }
     }
 }
