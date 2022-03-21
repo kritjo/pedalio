790111bd3afa81ba5fceea77f9b45d3b1f18a2a9
Jonas <jonasvox.2014@gmail.com>
2022-03-21 19:58:48 +0100

    Removed a useless function I wrote

--- app/src/main/java/in2000/pedalio/utils/CoordinateUtil.kt
+++ app/src/main/java/in2000/pedalio/utils/CoordinateUtil.kt
@@ -4,17 +4,13 @@ import com.tomtom.online.sdk.common.location.LatLng
 
 class CoordinateUtil {
     companion object{
-        fun XYtoLatLng(x: Double, y: Double): LatLng { // might be kind of useless
-            return LatLng(x, y)
-        }
-
         fun limitPointsOnRoute(points: List<LatLng>, maxPoints : Int): List<LatLng> {
             // naive implementation, in future we can use a more sophisticated algorithm which takes into account the distance between points
             if (points.size <= maxPoints) {
                 return points
             }
             val result = mutableListOf<LatLng>()
-            val step = points.size / maxPoints
+            val step : Int = points.size / maxPoints
             for (i in points.indices step step) {
                 result.add(points[i])
             }
