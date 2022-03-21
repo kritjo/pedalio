204aad6e9aaa7e749e78f9871a83da39b2f5138f
Jonas <jonasvox.2014@gmail.com>
2022-03-21 21:36:35 +0100

    Documentation

--- app/src/main/java/in2000/pedalio/data/routing/impl/TomtomRoutingRepository.kt
+++ app/src/main/java/in2000/pedalio/data/routing/impl/TomtomRoutingRepository.kt
@@ -9,6 +9,11 @@ import in2000.pedalio.data.routing.RoutingRepository
 import in2000.pedalio.data.routing.source.tomtom.TomtomRoutingSource
 
 class TomtomRoutingRepository(val context : Context) : RoutingRepository {
+    /**
+     * Calculates the route between several points (waypoints)
+     * @param waypoints the points to calculate the route between
+     * @return the route plan between the points
+     */
     override fun calculateRouteFromWaypoints(waypoints: List<LatLng>): RoutePlan {
         val source = TomtomRoutingSource(context)
         val route = source.getRouteFromLocations(waypoints)
@@ -19,6 +24,12 @@ class TomtomRoutingRepository(val context : Context) : RoutingRepository {
         }
     }
 
+    /**
+     * Calculates the route between two points
+     * @param from the starting point
+     * @param to the destination point
+     * @return the route plan between the points
+     */
     override fun calculateRoute(from: LatLng, to: LatLng): RoutePlan {
         val source = TomtomRoutingSource(context)
         val route = source.getRouteFromLocations(listOf(from, to))
