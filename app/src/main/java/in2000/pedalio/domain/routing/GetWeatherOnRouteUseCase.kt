ac647cfc1d7fd923c0d647a0480b85975d85ce05
Jonas <jonasvox.2014@gmail.com>
2022-03-21 20:00:22 +0100

    Get several weather data points on a list of locations (route)

--- app/src/main/java/in2000/pedalio/domain/routing/GetWeatherOnRouteUseCase.kt
+++ app/src/main/java/in2000/pedalio/domain/routing/GetWeatherOnRouteUseCase.kt
@@ -1,5 +1,22 @@
 package in2000.pedalio.domain.routing
 
-class GetWeatherOnRouteUseCase {
-    // TODO: Implement
+import com.tomtom.online.sdk.common.location.LatLng
+import in2000.pedalio.data.weather.WeatherRepository
+import in2000.pedalio.domain.weather.GetWeatherUseCase
+import in2000.pedalio.domain.weather.WeatherDataPoint
+
+class GetWeatherOnRouteUseCase(private val weatherRepository: WeatherRepository, private val getWeatherUseCase : GetWeatherUseCase) {
+    suspend fun getBatchWeather(locations: List<LatLng>) : List<WeatherDataPoint>? {
+        val weatherDataPoints = mutableListOf<WeatherDataPoint>()
+        for (location in locations) {
+            val weatherDataPoint = getWeatherUseCase.getWeather(location)
+            if (weatherDataPoint != null) {
+                weatherDataPoints.add(weatherDataPoint)
+            } else {
+                return null // this should never happen
+            }
+        }
+        return weatherDataPoints
+    }
 }
+
