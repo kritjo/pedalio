07fdf8e73686399e53f621db30a65b0e43af20ae
Jonas <jonasvox.2014@gmail.com>
2022-03-21 20:56:53 +0100

    Implemented calculateRoute and calculateRouteFromWaypoints for Tomtom

--- app/src/main/java/in2000/pedalio/data/routing/RoutingRepository.kt
+++ app/src/main/java/in2000/pedalio/data/routing/RoutingRepository.kt
@@ -1,8 +1,11 @@
 package in2000.pedalio.data.routing
 
+import android.content.Context
 import com.tomtom.online.sdk.common.location.LatLng
 import com.tomtom.online.sdk.map.Route
+import com.tomtom.online.sdk.routing.route.RoutePlan
 
 interface RoutingRepository { // Routing API from TomTom
-    fun calculateRoute(locations : List<LatLng>) : Route
+    fun calculateRouteFromWaypoints(locations : List<LatLng>) : RoutePlan
+    fun calculateRoute(from : LatLng, to : LatLng) : RoutePlan
 }
