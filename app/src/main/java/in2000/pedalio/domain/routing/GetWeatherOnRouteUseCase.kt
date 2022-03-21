57621b80cf549d1aead19376827c61a57af9c0a8
Jonas <jonasvox.2014@gmail.com>
2022-03-21 21:36:22 +0100

    Comments for getBatchWeather

--- app/src/main/java/in2000/pedalio/domain/routing/GetWeatherOnRouteUseCase.kt
+++ app/src/main/java/in2000/pedalio/domain/routing/GetWeatherOnRouteUseCase.kt
@@ -5,7 +5,13 @@ import in2000.pedalio.data.weather.WeatherRepository
 import in2000.pedalio.domain.weather.GetWeatherUseCase
 import in2000.pedalio.domain.weather.WeatherDataPoint
 
+
 class GetWeatherOnRouteUseCase(private val getWeatherUseCase : GetWeatherUseCase) {
+    /**
+     * Use case to get weather data for a route.
+     * @param getWeatherUseCase Use case to get weather data for a location.
+     * @return Weather data for a route.
+     */
     suspend fun getBatchWeather(locations: List<LatLng>) : List<WeatherDataPoint>? {
         val weatherDataPoints = mutableListOf<WeatherDataPoint>()
         for (location in locations) {
