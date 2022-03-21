1486c06ec6681703acbe718bcdaa5abf1a65e99d
Jonas Silva <jonaslsa@uio.no>
2022-03-21 20:56:53 +0100

    Implemented calculateRoute and calculateRouteFromWaypoints for Tomtom

--- app/src/main/java/in2000/pedalio/data/routing/impl/TomtomRoutingRepository.kt
+++ app/src/main/java/in2000/pedalio/data/routing/impl/TomtomRoutingRepository.kt
@@ -1,13 +1,31 @@
 package in2000.pedalio.data.routing.impl
 
+import android.content.Context
+import android.util.Log
 import com.tomtom.online.sdk.common.location.LatLng
 import com.tomtom.online.sdk.map.Route
+import com.tomtom.online.sdk.routing.route.RoutePlan
 import in2000.pedalio.data.routing.RoutingRepository
 import in2000.pedalio.data.routing.source.tomtom.TomtomRoutingSource
 
-class TomtomRoutingRepository : RoutingRepository {
-    override fun calculateRoute(locations: List<LatLng>): Route {
-        //val source = TomtomRoutingSource()
-        TODO("Not yet implemented")
+class TomtomRoutingRepository(val context : Context) : RoutingRepository {
+    override fun calculateRouteFromWaypoints(waypoints: List<LatLng>): RoutePlan {
+        val source = TomtomRoutingSource(context)
+        val route = source.getRouteFromLocations(waypoints)
+        if (route.isSuccess()) {
+            return route.value()
+        } else {
+            throw Exception("Error while calculating route: ${route.cause()}")
+        }
+    }
+
+    override fun calculateRoute(from: LatLng, to: LatLng): RoutePlan {
+        val source = TomtomRoutingSource(context)
+        val route = source.getRouteFromLocations(listOf(from, to))
+        if (route.isSuccess()) {
+            return route.value()
+        } else {
+            throw Exception("Error while calculating route: ${route.cause()}")
+        }
     }
 }
