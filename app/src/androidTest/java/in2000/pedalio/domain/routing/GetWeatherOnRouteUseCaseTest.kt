ca00e781defb81b32250dfa983102f18eaa01c92
Jonas <jonasvox.2014@gmail.com>
2022-03-25 11:52:00 +0100

    Fixed test according to new weather use case

--- app/src/androidTest/java/in2000/pedalio/domain/routing/GetWeatherOnRouteUseCaseTest.kt
+++ app/src/androidTest/java/in2000/pedalio/domain/routing/GetWeatherOnRouteUseCaseTest.kt
@@ -5,6 +5,7 @@ import com.tomtom.online.sdk.common.location.LatLng
 import com.tomtom.online.sdk.routing.route.RoutePlan
 import in2000.pedalio.data.Endpoints
 import in2000.pedalio.data.routing.impl.TomtomRoutingRepository
+import in2000.pedalio.data.weather.impl.LocationforecastRepository
 import in2000.pedalio.data.weather.impl.NowcastRepository
 import in2000.pedalio.domain.weather.GetWeatherUseCase
 import in2000.pedalio.utils.CoordinateUtil
@@ -25,8 +26,9 @@ class GetWeatherOnRouteUseCaseTest {
         val route = plan.routes[0]
         val routePoints = CoordinateUtil.limitPointsOnRouteSimple(route.getCoordinates(), 10)
         Assert.assertTrue(routePoints.size in 7..12)
-        val weatherRepo = NowcastRepository(Endpoints.NOWCAST_COMPLETE)
-        val weatherUseCase = GetWeatherUseCase(weatherRepo)
+        val nowcast = NowcastRepository(Endpoints.NOWCAST_COMPLETE)
+        val locationforecast = LocationforecastRepository(Endpoints.LOCATIONFORECAST_COMPLETE)
+        val weatherUseCase = GetWeatherUseCase(nowcast, locationforecast)
         val weatherOnRouteUseCase = GetWeatherOnRouteUseCase(weatherUseCase)
         runBlocking {
             val weatherOnRoute = weatherOnRouteUseCase.getBatchWeather(routePoints)
