6f5162d7fa48d2523e382577a9b9f0eb7d1dc53f
omarelkadi96 <79992709+omarelkadi96@users.noreply.github.com>
2022-04-01 13:54:05 +0200

    La til jitpack for å kunne bruke drawable icons i Edittext som button Lagt til string recourses Ny aktivitet for søk

---
+++ app/src/main/java/in2000/pedalio/SearchActivity.kt
@@ -0,0 +1,12 @@
+package in2000.pedalio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+
+    }
+}
