1072dba20a467ea87e77fe0b25360d5567438b3e
omarelkadi96 <79992709+omarelkadi96@users.noreply.github.com>
2022-04-01 13:54:23 +0200

    La til jitpack for å kunne bruke drawable icons i Edittext som button Lagt til string recourses Ny aktivitet for søk

--- app/src/main/java/in2000/pedalio/MainActivity.kt
+++ app/src/main/java/in2000/pedalio/MainActivity.kt
@@ -18,7 +18,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val navHostFragment = supportFragmentManager.findFragmentById(
             R.id.nav_host_container
         ) as NavHostFragment
@@ -33,6 +32,7 @@ class MainActivity : AppCompatActivity() {
             setOf(R.id.titleScreen)
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
