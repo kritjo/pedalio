31972f5f59bcd02555229af23cfbdbbf55995a9a
Kristian Tjelta Johansen <kritjo@kritjo.com>
2022-04-20 17:36:52 +0200

    BikeRoute KDoc
    Signed-off-by: Kristian Tjelta Johansen <kritjo@kritjo.com>

--- app/src/main/java/in2000/pedalio/data/bikeRoutes/impl/OsloBikeRouteRepostiory.kt
+++ app/src/main/java/in2000/pedalio/data/bikeRoutes/impl/OsloBikeRouteRepostiory.kt
@@ -10,7 +10,13 @@ import mil.nga.proj.Projection
 import mil.nga.proj.ProjectionFactory
 import mil.nga.proj.ProjectionTransform
 
+/**
+ * Oslo Kommune's bike routes
+ */
 class OsloBikeRouteRepostiory(val endpoint: String) : BikeRouteRepository() {
+    /**
+     * @see [BikeRouteRepository.getRoutes]
+     */
     override suspend fun getRoutes(): List<List<LatLng>> {
         val stringRoutes = OsloBikeRoutesSource.getRoutes(endpoint)
             // remove non RFC7946 compliant data
@@ -22,6 +28,9 @@ class OsloBikeRouteRepostiory(val endpoint: String) : BikeRouteRepository() {
         val epsg4326: Projection? = ProjectionFactory.getProjection(4326)
         val transformation: ProjectionTransform = epsg25832!!.getTransformation(epsg4326)
 
+        // Convert the string to a FeatureCollection and then get the features
+        // The features are the routes in the GeoJSON format, so convert them to LineStrings
+        // and then convert them to a list of lists of LatLng
         return (stringRoutes.toGeoJsonObject() as FeatureCollection).features.map { feature ->
             (feature.geometry as LineString).coordinates.map {
                 val projCord: DoubleArray? = transformation.transform(it[0], it[1])
