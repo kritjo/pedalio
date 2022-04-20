517de866d58f5f253f6a226df626bc95909fba34
Kristian Tjelta Johansen <kritjo@kritjo.com>
2022-04-20 18:30:58 +0200

    Revert "fix: Documentation" Not correct.
    This reverts commit be62e7bd
    
    Signed-off-by: Kristian Tjelta Johansen <kritjo@kritjo.com>

--- app/src/main/java/in2000/pedalio/ui/map/Bubble.kt
+++ app/src/main/java/in2000/pedalio/ui/map/Bubble.kt
@@ -2,10 +2,7 @@ package in2000.pedalio.ui.map
 
 import android.view.View
 import com.tomtom.online.sdk.common.location.LatLng
-/**
- * A bubble that can be displayed on the map.
- * @param latLng the location of the bubble
- */
+
 interface Bubble {
     val latLng: LatLng
     val button: View
