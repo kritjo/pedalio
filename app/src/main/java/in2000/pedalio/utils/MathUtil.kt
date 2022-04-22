6b50726da3c8073331f8f4bd414245c6d626bd91
Jonas <jonasvox.2014@gmail.com>
2022-04-22 14:05:07 +0200

    Removed useless func

--- app/src/main/java/in2000/pedalio/utils/MathUtil.kt
+++ app/src/main/java/in2000/pedalio/utils/MathUtil.kt
@@ -1,6 +1,4 @@
 package in2000.pedalio.utils
-import android.graphics.PointF
-import com.google.android.material.math.MathUtils.dist
 import com.tomtom.online.sdk.common.location.LatLng
 import kotlin.math.pow
 
@@ -9,24 +7,11 @@ class MathUtil {
     companion object {
       // Docs : https://gisgeography.com/inverse-distance-weighting-idw-interpolation/#:~:text=Interpolated%20points%20are%20estimated%20based,has%20from%20the%20known%20values.
         fun inverseDistanceWeighting(here : LatLng, stations : List<Pair<LatLng, Double>>): Double {
-          val POWER = 2;
+          val POWER = 2
           var sum = 0.0
           var sumDx = 0.0
           for (i in stations.indices) {
             val dist = CoordinateUtil.calcDistanceBetweenTwoCoordinates(here, stations[i].first)
-            val value = stations[i].second
-            sum += value / dist.pow(POWER)
-            sumDx += 1.0 / dist.pow(POWER)
-          }
-
-          return sum / sumDx
-        }
-        fun inverseDistanceWeighting(here : PointF, stations : List<Pair<PointF, Double>>): Double {
-          val POWER = 2;
-          var sum = 0.0
-          var sumDx = 0.0
-          for (i in stations.indices) {
-            val dist = dist(here.x, here.y, stations[i].first.x, stations[i].first.y)
             val value = stations[i].second
             sum += value / dist.pow(POWER)
             sumDx += 1.0 / dist.pow(POWER)
